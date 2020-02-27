f12a5e99deeeb03b792a9ece1e33c22da8abc6a9
patria gani <patria.gani@gmail.com>
2020-02-27 14:08:23 +0700

    add login page and sign up page

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>homepage</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
